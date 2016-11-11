37e0362681b4f897d4cdd859dcfee6be3d5f472e
Sumwunn <0x77VA@gmail.com>
2016-11-11 17:36:00 -0500

    Now supporting multiple code patterns.

--- AchievementsModsEnabler/AchievementsModsEnabler.cpp
+++ AchievementsModsEnabler/AchievementsModsEnabler.cpp
@@ -26,11 +26,13 @@ CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.*/
 #include <string>
 
 // Defined functions.
-// ASM.
+// ASM
 extern "C" void* BinSearch(void* Search, int SearchLength, unsigned char* Bytes, int BytesLength, int AddMod, int SubMod);
 // Work around because my ASM function GetTextSectionData has multiple return value data types.
 extern "C" void* GetTextSectionAddr(HMODULE Module, int DataType);
 extern "C" int GetTextSectionSize(HMODULE Module, int DataType);
+// C++
+int BinPatch(HMODULE hModule, unsigned char* BytesToFind, int BytesToFindSize, unsigned char* BytesPatch, int BytesPatchSize, int AddressModifierAdd, int AddressModifierSub);
 
 // Return values
 // 0 = Patching failed, bytes not found.
@@ -48,15 +50,84 @@ extern "C" __declspec(dllexport) int Setup() {
 
 	LPCTSTR ExpectedProcess02 = L"SkyrimSE.exe";
 	// These bytes will land us just beneath where the achivements mods disabler code is at.
-	unsigned char BytesToFind02[] = { 0xC3, 0x40, 0x32, 0xFF, 0x48, 0x89, 0x5C, 0x24, 0x40, 0x48, 0x89, 0x6C, 0x24, 0x48 };
+	unsigned char BytesToFind02_01[] = { 0xC3, 0x48, 0x89, 0x5C, 0x24, 0x40, 0x48, 0x89, 0x6C, 0x24, 0x48, 0x8B, 0xA9, 0x70, 0x0D, 0x00, 0x00 }; // Skyrim SE v1.1.
+	unsigned char BytesToFind02_02[] = { 0xC3, 0x40, 0x32, 0xFF, 0x48, 0x89, 0x5C, 0x24, 0x40, 0x48, 0x89, 0x6C, 0x24, 0x48 }; // Skyrim SE v1.2.
 	// This is what we patch it with (check notes.txt).
 	unsigned char BytesPatch02[] = { 0xB0, 0x00, 0xC3 };
 
-	// Pointers.
-	unsigned char *BytesToFind = NULL;
-	unsigned char *BytesPatch = NULL;
-	int BytesToFindSize = NULL;
-	int BytesPatchSize = NULL;
+	// We need to go back X bytes so we land at the right address.
+	int AddressModifierSub01 = 0x29; // Fallout 4.
+	int AddressModifierSub02_01 = 0x35; // Skyrim SE v1.1.
+	int AddressModifierSub02_02 = 0x30; // Skyrim SE v1.2.
+
+	// Misc.
+	HMODULE hModule = NULL;
+	std::ofstream LogFileHandle;
+
+	//////// Setup Part 1 - Addresses & Logging ////////
+
+	// Fallout 4.
+	hModule = GetModuleHandle(ExpectedProcess01);
+	if (hModule != NULL) {
+		// Open up fresh log file.
+		LogFileHandle.open(L"AchievementsModsEnabler.log");
+		// Log file creation failed.
+		if (!LogFileHandle) {
+			return -2;
+		}
+		// Find bytes and patch them.
+		if (BinPatch(hModule, BytesToFind01, sizeof BytesToFind01, BytesPatch01, sizeof BytesPatch01, NULL, AddressModifierSub01) == 0) {
+			// Bytes not found!
+			// Log message.
+			LogFileHandle << "NO" << std::endl;
+			// Cleanup.
+			LogFileHandle.close();
+			return 0;
+		}
+		else {
+			// Bytes found!
+			// Log message.
+			LogFileHandle << "YES" << std::endl;
+			// Cleanup.
+			LogFileHandle.close();
+			return 1;
+		}
+	}
+
+	// Skyrim SE.
+	hModule = GetModuleHandle(ExpectedProcess02);
+	if (hModule != NULL) {
+		// Open up fresh log file.
+		LogFileHandle.open(L"Data\\Plugins\\Sumwunn\\AchievementsModsEnabler.log");
+		// Log file creation failed.
+		if (!LogFileHandle) {
+			return -2;
+		}
+		// Find bytes and patch them.
+		if (BinPatch(hModule, BytesToFind02_01, sizeof BytesToFind02_01, BytesPatch02, sizeof BytesPatch02, NULL, AddressModifierSub02_01) == 0 &&
+			BinPatch(hModule, BytesToFind02_02, sizeof BytesToFind02_02, BytesPatch02, sizeof BytesPatch02, NULL, AddressModifierSub02_02) == 0) {
+			// Bytes not found!
+			// Log message.
+			LogFileHandle << "NO" << std::endl;
+			// Cleanup.
+			LogFileHandle.close();
+			return 0;
+		}
+		else {
+			// Bytes found!
+			// Log message.
+			LogFileHandle << "YES" << std::endl;
+			// Cleanup.
+			LogFileHandle.close();
+			return 1;
+		}
+	}
+
+	// Process not found.
+	return -1;
+}
+
+int BinPatch(HMODULE hModule, unsigned char* BytesToFind, int BytesToFindSize, unsigned char* BytesPatch, int BytesPatchSize, int AddressModifierAdd, int AddressModifierSub) { // BinSearch + MEMCPY patching.
 
 	// The address we get from GetTextSectionAddr.
 	void* SearchAddress = (void*)NULL;
@@ -64,85 +135,26 @@ extern "C" __declspec(dllexport) int Setup() {
 	int SearchSize = NULL;
 	// The address we get from BinSearch.
 	void* PatchAddress = (void*)NULL;
-	// We need to go back X bytes so we land at the right address.
-	int PatchAddressModifier = NULL;
-	int PatchAddressModifier01 = 0x29; // Fallout 4.
-	int PatchAddressModifier02 = 0x30; // Skyrim SE.
-
-	// Logging.
-	LPCTSTR LogFilePath = NULL;
-	LPCTSTR LogFilePath01 = L"AchievementsModsEnabler.log"; // Non-NMM log path.
-	LPCTSTR LogFilePath02 = L"Data\\Plugins\\Sumwunn\\AchievementsModsEnabler.log";  // NMM log path.
-
 	// Misc.
-	DWORD OldVP = NULL;
-
-	//////// Setup Part 1 - Addresses & Logging ////////
-
-	// Get hModule of target to patch.
-	// Fallout4.exe
-	HMODULE TargetModule = GetModuleHandle(ExpectedProcess01);
-	if (TargetModule != NULL) {
-		// Setup custom addresses for detected process.
-		LogFilePath = LogFilePath01;
-		PatchAddressModifier = PatchAddressModifier01;
-		BytesToFind = BytesToFind01;
-		BytesPatch = BytesPatch01;
-		BytesToFindSize = sizeof BytesToFind01;
-		BytesPatchSize = sizeof BytesPatch01;
-	}
-	// SkyrimSE.exe
-	else {
-		TargetModule = GetModuleHandle(ExpectedProcess02);
-		if (TargetModule != NULL) {
-			// Setup custom addresses for detected process.
-			LogFilePath = LogFilePath02;
-			PatchAddressModifier = PatchAddressModifier02;
-			BytesToFind = BytesToFind02;
-			BytesPatch = BytesPatch02;
-			BytesToFindSize = sizeof BytesToFind02;
-			BytesPatchSize = sizeof BytesPatch02;
-		}
-		// No expected processes detected!
-		else if (TargetModule == NULL) {
-			return -1;
-		}
-	}
-
-	// Open up fresh log file.
-	std::ofstream LogFile;
-	LogFile.open(LogFilePath);
-	// Log file creation failed.
-	if (!LogFile) {
-		return -2;
-	}
+	DWORD lpflOldProtect = NULL;
 
 	// Get size and address of ExpectedProcess's .text section.
-	SearchSize = GetTextSectionSize(TargetModule, 1);
-	SearchAddress = GetTextSectionAddr(TargetModule, 2);
-
+	SearchSize = GetTextSectionSize(hModule, 1);
+	SearchAddress = GetTextSectionAddr(hModule, 2);
 	// Get address and patch it.
-	PatchAddress = BinSearch(SearchAddress, SearchSize, BytesToFind, BytesToFindSize, NULL, PatchAddressModifier);
-	// Bytes not found!
+	PatchAddress = BinSearch(SearchAddress, SearchSize, BytesToFind, BytesToFindSize, AddressModifierAdd, AddressModifierSub);
 	if (PatchAddress == NULL) {
-		// Log message.
-		LogFile << "NO" << std::endl;
+		// Bytes not found.
+		return 0;
 	}
 	// Bytes found!
 	else {
 		// Patch it! (with NOPS)
-		VirtualProtect(PatchAddress, BytesPatchSize, PAGE_EXECUTE_READWRITE, &OldVP);
+		VirtualProtect(PatchAddress, BytesPatchSize, PAGE_EXECUTE_READWRITE, &lpflOldProtect);
 		memcpy(PatchAddress, BytesPatch, BytesPatchSize);
-		VirtualProtect(PatchAddress, BytesPatchSize, OldVP, &OldVP);
-		// Log message.
-		LogFile << "YES" << std::endl;
-		// Cleanup.
-		LogFile.close();
+		VirtualProtect(PatchAddress, BytesPatchSize, lpflOldProtect, &lpflOldProtect);
 		return 1;
 	}
-
-	// Cleanup.
-	LogFile.close();
 
 	return 0;
 }
